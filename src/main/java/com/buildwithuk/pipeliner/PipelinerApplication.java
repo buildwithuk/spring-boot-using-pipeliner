b64ca5b46c2787b82aec9700c5cb8be5117989e8
Umair Khan <umairk83@live.com>
2023-07-08 01:31:27 +0500

    Added initial project code

---
+++ src/main/java/com/buildwithuk/pipeliner/PipelinerApplication.java
@@ -0,0 +1,13 @@
+package com.buildwithuk.pipeliner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelinerApplication.class, args);
+	}
+
+}
